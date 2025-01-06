f7c6ec31151a2de7ad2c48bada911815a6c29418
Suraj Kumar <surajkumar06174@gmail.com>
2025-01-06 23:45:38 +0530

    new file:   .vscode/settings.json 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
+// stickyNavbar.js
+
 let lastScrollPosition = 0;
-  const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector('.navbar');
+const sectionHeroEl = document.querySelector(".hero");
 
-  window.addEventListener('scroll', () => {
-    const currentScrollPosition = window.pageYOffset;
+// Intersection Observer to add sticky class when the hero section is out of view
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
 
-    if (currentScrollPosition > lastScrollPosition) {
-      // Scrolling down
-      navbar.style.transform = 'translateY(-100%)';
-    } else {
-      // Scrolling up
-      navbar.style.transform = 'translateY(0)';
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
     }
 
-    lastScrollPosition = currentScrollPosition;
-  });
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);  // Observing the .hero section
+
+// Scroll functionality to hide/show navbar on scroll
+window.addEventListener('scroll', () => {
+  const currentScrollPosition = window.pageYOffset;
+
+  if (currentScrollPosition > lastScrollPosition) {
+    // Scrolling down, hide navbar
+    navbar.style.transform = 'translateY(-100%)';
+    navbar.style.transition = 'transform 250ms ease-in';
+  } else {
+    // Scrolling up, show navbar
+    navbar.style.transform = 'translateY(0)';
+    navbar.style.transition = 'transform 250ms ease-in';
+  }
+
+  lastScrollPosition = currentScrollPosition;
+});
